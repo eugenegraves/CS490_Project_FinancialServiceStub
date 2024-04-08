9bb05e90191929949ab6e1e69ccb7cc4bdf0d779
eugenegraves <elg22@njit.edu>
2024-04-07 21:33:16 -0400

    Communication between backend and financial stub complete

--- server.py
+++ server.py
@@ -4,6 +4,14 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/receive_finance_application', methods=['POST'])
+def receiveFinanceApp():
+    try:
+        data = request.get_json()
+        print(data)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    return jsonify({'message': 'Stub has recieved application'}), 201
 
 if __name__ == "__main__":
     app.run(debug = True, host='localhost', port='5001')
