10197dff9f1a303d0a8c82b5ad095de4d2bb6bea
eugenegraves <elg22@njit.edu>
2024-04-11 17:58:34 -0400

    Finance App Logic Updated (Partial)

--- server.py
+++ server.py
@@ -9,9 +9,17 @@ def receiveFinanceApp():
     try:
         data = request.get_json()
         print(data)
+        response = computeAndSendDecision(data)
+        print(response)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     return jsonify({'message': 'Stub has recieved application'}), 201
+def computeAndSendDecision(data):
+    #print(type(request.get_json()["full_name"]))
+    print(data["annual_income"])
+    if (data["annual_income"] >= int(data["purchase_price"]) * 0.1):
+        print("QUALIFIED SO FAR")
+    return "HI"
 
 if __name__ == "__main__":
     app.run(debug = True, host='localhost', port='5001')
